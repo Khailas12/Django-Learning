c89c7d323e8bab9ae23049a37b3d75865cb0f233
Khailas R <khailas303@gmail.com>
2021-09-29 21:46:32 +0530

    update

--- src/mypage/views.py
+++ src/mypage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return HttpResponse('<h1>Hello World</h1>')
