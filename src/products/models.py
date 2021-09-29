17d2447242a283df7529b85c158e283fceabcaf6
Khailas R <khailas303@gmail.com>
2021-09-29 19:45:48 +0530

    update

--- src/products/models.py
+++ src/products/models.py
@@ -7,3 +7,5 @@ class Product(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10000000)
     summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(null=True)   # null=True or default=True
+    # null -> purely database-related
+    # blank -> validation-related. If a field has blank=True , form validation will allow entry of an empty value. If a field has blank=False, the field will be required.
