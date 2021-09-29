fba5f107f345aedc5779d427b439d19295d2b212
Khailas R <khailas303@gmail.com>
2021-09-29 23:44:56 +0530

    updated

--- src/mypage/views.py
+++ src/mypage/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
-    print(request.user) # this shows the username in the terminal and shows anonymous in incognito mode
+    # this shows the username in the terminal and shows anonymous in incognito mode
+    print(request.user)
     # return HttpResponse('<h1>Hello World</h1>')
     return render(request, "home.html", {})
 
-def content_view(request, *args, **kwargs):
-    return render(request, "content.html", {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about", {})
+    return render(request, "about.html", {})
